9afc4a41849758a021ebcf1df974bdb6011675a6
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-09-20 13:30:50 -0400

    Created a function for output
    Created a function to deal with the output rather than manually outputing

--- Pomodoro_Timer.cpp
+++ Pomodoro_Timer.cpp
@@ -16,10 +16,15 @@ using namespace std;
  *	void break_sesh -- Postcondition - output to console that the break has begun and then, 
  *				using the chrono libary, it starts a timer for 5 minutes.
  *
- *	int user_input -- Precondition - called only after either study_sesh or break_sesh have
+ *	void user_input -- Precondition - called only after either study_sesh or break_sesh have
  *				been completed.
  *				Postcondition - If the user enters 1 the program will continue,
  *				if not then program will exit using assert.	    
+ *
+ *	void timer_out -- Precondition - called when wanting to tell the user that a session has
+ *				has ended
+ *			  Postcondition - this funtion will output if what session has ended and
+ *
  */
 int sesh_track = 1;// counter for sessions completed
 int break_track = 1;// counter for breaks completed
@@ -41,63 +46,69 @@ void user_input(){
 	int x;
 	cout << "Please enter 1 to continue" << endl;
 	cin >> x;
+	cout << " " << endl;
 
 	switch (x){
 		case 1:
 			return;
 		default:
 			assert(x == 1);
-
 	}
 
+}
+
+void timer_out(){
+	if (sesh_track == 1){
+                cout << "Welcome to Pomodoro timer" << endl;
+		cout << " " << endl;
+		return;
+	}
+	if (sesh_track > break_track){
+		cout << "Study session " << sesh_track - 1 << " has ended" << endl;
+		cout << " " << endl;
+	}
+	if (sesh_track == break_track){
+		cout << "Break " << break_track - 1 << " has ended" << endl;
+		cout << " " << endl;
+	}
+	if (sesh_track == 5){
+		cout << "Congradulations!, You have completed one Pomodoro cycle." << endl;
+	}
+	else
+		return;
 }
 
 
 int main(){
 
-	cout << "Welcome to the Pomodoro timer!" << endl;
-	cout << " " << endl;
-	
+	timer_out();
 	study_sesh();
-	cout << " " << endl;
-	cout << "Study session 1 has ended" << endl;
-	cout << " " << endl;
+	timer_out();
 	
 	user_input();
 	break_sesh();
-	cout << " " << endl;
-	cout << "Break 1 has ended" << endl;
-	cout << " " << endl;
-	
+	timer_out();
+		
 	user_input();
 	study_sesh();
-	cout << " " << endl;
-	cout << "Study Session 2 has ended" << endl;
-	cout << " " << endl;
+	timer_out();
 
 	user_input();
 	break_sesh();
-	cout << " " << endl;
-	cout << "Break 2 has ended" << endl;
-	cout << " " << endl;
+	timer_out();
 
 	user_input();
 	study_sesh();
-	cout << " " << endl;
-	cout << "Study Session 3 has ended" << endl;
-	cout << " " << endl;
+	timer_out();
 
 	user_input();
 	break_sesh();
-	cout << " " << endl;
-	cout << "Break 3 was ended" << endl;
-	cout << " " << endl;
+	timer_out();
 
 	user_input();
 	study_sesh();
-	cout << " " << endl;
-	cout << "Congradulations you have finished a Pomodoro cycle, a 25 minute break is advised before continuing" << endl;
-	
+	timer_out();
+
+
 	return 0;
 }
-
